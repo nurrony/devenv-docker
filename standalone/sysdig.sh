30699fe0507f41245e59ae594d51b66d9d9958f6
Nur Rony <pro.nmrony@gmail.com>
2016-08-26 00:15:17 +0600

    feat(sysdig): adds new standalone sysdig container

---
+++ standalone/sysdig.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sysdig() {
+  del_stopped sysdig-container
+  docker run -it \
+  --name sysdig-container \
+  --privileged \
+  -v /var/run/docker.sock:/host/var/run/docker.sock \
+  -v /dev:/host/dev -v /proc:/host/proc:ro \
+  -v /boot:/host/boot:ro \
+  -v /lib/modules:/host/lib/modules:ro \
+  -v /usr:/host/usr:ro \
+  sysdig/sysdig
+}
