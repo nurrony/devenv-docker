6a9dd9215fb7eb90a87559f2c31d8e6ef40a8066
Nur Rony <pro.nmrony@gmail.com>
2016-08-27 13:10:29 +0600

    chore(style): reformatting sysdig function

--- standalone/sysdig.sh
+++ standalone/sysdig.sh
@@ -3,12 +3,13 @@
 sysdig() {
   del_stopped sysdig-container
   docker run -it \
-  --name sysdig-container \
-  --privileged \
-  -v /var/run/docker.sock:/host/var/run/docker.sock \
-  -v /dev:/host/dev -v /proc:/host/proc:ro \
-  -v /boot:/host/boot:ro \
-  -v /lib/modules:/host/lib/modules:ro \
-  -v /usr:/host/usr:ro \
-  sysdig/sysdig
+    --name sysdig-container \
+    --privileged \
+    -v /var/run/docker.sock:/host/var/run/docker.sock \
+    -v /dev:/host/dev \
+    -v /proc:/host/proc:ro \
+    -v /boot:/host/boot:ro \
+    -v /lib/modules:/host/lib/modules:ro \
+    -v /usr:/host/usr:ro \
+    sysdig/sysdig
 }
