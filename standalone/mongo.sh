9343b1d4714fbc966f0137a9c13ec51e4087ece7
Nur Rony <pro.nmrony@gmail.com>
2016-09-11 13:30:39 +0600

    fix(mongodb): fixes container state detection issues

--- standalone/mongo.sh
+++ standalone/mongo.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-mongodb() {
-  # first check mongodb container is running or not
-  del_stopped mongo-server
-  local state=$(is_running mongo-server)
+mongodb(){
+  del_stopped mongodb
+  local state=$(docker inspect --format "{{.State.Running}}" mongodb 2>/dev/null)
 
-  if [[ "$state" == "true" ]]; then
-    echo 'mongodb server container is already running'
-  else
+  if [[ "$state" == "false" ]] || [[ "$state" == "" ]]; then
+    echo "mongodb server is not running, starting it for you."
     docker run -it -d \
       -v ${DEV_ZONE}/storage/mongodb:/data/db \
       -p 27017:27017 \
       --net ronsvpn \
-      --name mongodb-server \
-      mvertes/alpine-mongo
+      --name mongodb \
+      mvertes/alpine-mongo > /dev/null 2>&1
+  else
+    echo 'mongodb is already running'
   fi
 }
