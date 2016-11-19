608e3e9bb0b44b4c4240a2aef6b505ddda45377e
Nur Rony <pro.nmrony@gmail.com>
2016-11-19 21:26:16 +0600

    corrects mount path and configuration for personal blog

--- blog/config.js
+++ blog/config.js
@@ -13,7 +13,7 @@ config = {
         database: {
             client: 'mysql',
             connection: {
-                host     : 'mysql-dev',
+                host     : 'mysql',
                 user     : 'root',
                 password : 'nmrony',
                 database : 'ghost_nmrony_info',
@@ -32,7 +32,7 @@ config = {
         database: {
             client: 'mysql',
             connection: {
-                host     : 'mysql-dev',
+                host     : 'mysql',
                 user     : 'root',
                 password : 'nmrony',
                 database : 'ghost_nmrony_info',
